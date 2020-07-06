cdac70ecbf12ea6a609a233e022b125d354ca2ef
darshanraju <darshan.raju99@gmail.com>
2020-07-06 19:09:24 +1000

    first version of tile and piece class

--- Tile.java
+++ Tile.java
@@ -1,2 +1,54 @@
-public class Tile {
+public abstract class Tile {
+
+    int tileCoordinate;
+
+    Tile(int tileCoordinate){
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    //is tile occupied
+    public abstract boolean isTileOccupied();
+
+    //Piece on tile
+    public abstract Piece getPiece();
+
+    //Empty tile child class
+    public static final class EmptyTile extends Tile{
+        EmptyTile(int cooridnate) {
+            super(cooridnate);
+        }
+
+        @Override
+        public boolean isTileOccupied(){
+            return false;
+        }
+
+        @Override
+        public Piece getPiece(){
+            return null;
+        }
+    }
+
+
+
+    //Occupied tile child class
+    public static final class OccupiedTile extends Tile{
+
+        Piece pieceOnTile;
+
+        OccupiedTile(int cooridnate, Piece pieceOnTile) {
+            super(cooridnate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied(){
+            return true;
+        }
+
+        @Override
+        public Piece getPiece(){
+            return this.pieceOnTile;
+        }
+    }
 }
